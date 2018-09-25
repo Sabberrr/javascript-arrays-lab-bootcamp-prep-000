b5270213fa2a139b3c6216152a4cae5d31336412
Jan Spencer Catalan <jancatalan2@gmail.com>
2018-09-25 04:38:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens;
+}
+function appendKitten(name) {
+  var array2 = kittens;
+  array2.push(name);
+  return array2;
+}
+function prependKitten(name) {
+  var array2 = kittens;
+  array2.unshift(name);
+  return array2;
+}
